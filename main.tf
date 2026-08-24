3de8e2317c66e8cfec42d258c553105f5bb4dbe2
sivatejasurathula2003-art <sivatejasurathula2003@gmail.com>
2026-08-24 17:28:48 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+region = "us-east-1"
+}
+
+resource "aws_instance" "one" {
+count = 4
+ami = "ami-04aa00acb1165b32a"
+instance_type = "t2.medium"
+key_name = "argocd"
+vpc_security_group_ids = ["sg-06785bc60f40dceda"]
+tags = {
+Name = var.instance_names[count.index]
+}
+}
+
+variable "instance_names" {
+default = ["jenkins", "APPSERVER-1", "APPSERVER-2", "Monitoring server"]
+}
